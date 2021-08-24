87dd9b389b9d779a744dd565e485276fb5207cbb
kyohei uno <waizeru00@gmail.com>
2021-08-25 08:38:34 +0900

    フォームのコンソール出力機能の作成

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("この表示が出ていれば読み込みできています。")
+const form = document.getElementById("todoForm")
+const input = document.getElementById("todoInput")
+
+//入力した値をコンソールに出力させる
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); //送信時の画面リロードをなくす
+  console.log(input.value);
+})
